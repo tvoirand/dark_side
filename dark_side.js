9a78504db18dd90bf0417656f364ec863c54e4d3
Bielorusse <thibautvoirand@gmail.com>
2020-01-29 20:09:50 +0100

    linked earth and moon positions to spice data files and added a slider control

--- dark_side.js
+++ dark_side.js
@@ -10,8 +10,11 @@ import { SpiceSimulation } from "./modules/spice_simulation.js";
 import { Sun } from "./modules/graphics/sun.js";
 import { Planet } from "./modules/graphics/planet.js";
 import { init_webgl_context } from "./modules/graphics/webgl_utils.js";
-
-var spice = new SpiceSimulation();
+import {
+    norm,
+    scalar_vector_multiplication,
+    add_vectors_elementwise
+} from "./modules/utils.js";
 
 main();
 
@@ -19,6 +22,9 @@ function main() {
     /*
     Main function for the dark_side project.
     */
+
+    // read times and earth and moon positions from spice data files
+    var spice_data = read_spice_data();
 
     // initiate canvas
     const canvas = document.querySelector("canvas");
@@ -32,42 +38,63 @@ function main() {
     var projection_matrix = init_webgl_context(gl);
 
     // initiate objects to draw
-    var earth = new Planet(1.25, "EARTH", [0.2, 0.2, 1.0, 1.0], "SUN", gl);
-    var moon = new Planet(0.75, "MOON", [0.9, 0.9, 0.9, 1.0], "EARTH", gl);
-    var sun = new Sun(1.0, [0, 0, -20], "SUN", [1.0, 1.0, 0.0, 1.0], "SUN", gl);
+    var earth = new Planet(
+        1.25, // radius
+        "EARTH", // name
+        [0.2, 0.2, 1.0, 1.0], // color
+        0.00000004, // orbit display factor
+        "SUN", // central body
+        gl // webgl context
+    );
+    var moon = new Planet(
+        0.75, // radius
+        "MOON", // name
+        [0.9, 0.9, 0.9, 1.0], // color
+        0.000006, // orbit display factor
+        "EARTH", // central body
+        gl // webgl context
+    );
+    var sun = new Sun(
+        1.0, // radius
+        [0, 0, -20], // position
+        "SUN", // name
+        [1.0, 1.0, 0.0, 1.0], // color
+        "SUN", // central body
+        gl // webgl context
+    );
     var objects_to_draw = [earth, moon, sun];
-
-    // initiate elapsed time
-    var elapsed_time = 0;
 
     // loop animation function called at each display refresh
     var render = function(time) {
-        time *= 0.001;
-        const delta_t = time - elapsed_time;
-        elapsed_time = time;
+        /*
+        Input:
+            -time   float
+                time since last refresh (in ms) ?
+        */
 
-        // update objects positions
-        earth.position = spice.spkpos(
-            earth.name,
-            time,
-            "ECLIPJ2000",
-            "NONE",
-            earth.central_body
+        // get index for spice data arrays based on the time slider value
+        var index = get_current_index(spice_data.times.length);
+
+        // display time
+        document.getElementById("time_display").innerHTML =
+            spice_data.times[index];
+
+        // update earth and moon positions
+        earth.position = scalar_vector_multiplication(
+            earth.orbit_display_factor,
+            spice_data.earth_positions[index]
         );
-        moon.position = spice.spkpos(
-            moon.name,
-            time,
-            "ECLIPJ2000",
-            "NONE",
-            moon.central_body
+        moon.position = add_vectors_elementwise(
+            earth.position,
+            scalar_vector_multiplication(
+                moon.orbit_display_factor,
+                spice_data.moon_positions[index]
+            )
         );
-        sun.position = spice.spkpos(
-            sun.name,
-            time,
-            "ECLIPJ2000",
-            "NONE",
-            sun.central_body
-        );
+
+        // shift earth and moon Z value
+        earth.position[2] -= 20;
+        moon.position[2] -= 20;
 
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
@@ -79,4 +106,94 @@ function main() {
         requestAnimationFrame(render);
     };
     requestAnimationFrame(render);
+}
+
+function get_current_index(arrays_length) {
+    /*
+    Get current index for data array, given the value of the time slider.
+    Input:
+        -arrays.length  int
+    Output:
+        -               int
+    */
+
+    // get slider values
+    var slider_value = document.getElementById("time_slider").value;
+    var slider_max = document.getElementById("time_slider").max;
+    var slider_min = document.getElementById("time_slider").min;
+
+    // compute corresponding index in times array
+    return Math.floor((slider_value - slider_min) * arrays_length / slider_max);
+}
+
+function read_spice_data() {
+    /*
+    Read times and objects (earth and moon) positions from files computed with the scipeypy script.
+    Output:
+        -spice_data     javascript object
+            contains:
+                -times: [float, ...]
+                -earth_positions: [float, ...]
+                -moon_positions: [float, ...]
+    */
+
+    // initiate arrays to store times, earth positions, and moon positions data
+    var times = [];
+    var earth_positions = [];
+    var moon_positions = [];
+
+    // fetch times through ajax request
+    $.ajax({
+        type: "GET",
+        url: "./data/times.txt",
+        success: function(data) {
+            var rows = data.split("\n");
+            for (var i = 1; i < rows.length; i++) {
+                times.push(parseFloat(rows[i]));
+            }
+        }
+    });
+
+    // fetch earth positions through ajax request
+    $.ajax({
+        type: "GET",
+        url: "./data/earth.txt",
+        success: function(data) {
+            var rows = data.split("\n");
+            for (var i = 1; i < rows.length; i++) {
+                var cols = rows[i].split(" ");
+                earth_positions.push([
+                    parseFloat(cols[0]),
+                    parseFloat(cols[1]),
+                    parseFloat(cols[2])
+                ]);
+            }
+        }
+    });
+
+    // fetch moon positions through ajax request
+    $.ajax({
+        type: "GET",
+        url: "./data/moon.txt",
+        success: function(data) {
+            var rows = data.split("\n");
+            for (var i = 1; i < rows.length; i++) {
+                var cols = rows[i].split(" ");
+                moon_positions.push([
+                    parseFloat(cols[0]),
+                    parseFloat(cols[1]),
+                    parseFloat(cols[2])
+                ]);
+            }
+        }
+    });
+
+    // create output object
+    var spice_data = {
+        times: times,
+        earth_positions: earth_positions,
+        moon_positions: moon_positions
+    };
+
+    return spice_data;
 }
