6702c28bd0ff4e9760b9e182e06c7fc52800ddfb
Bielorusse <thibautvoirand@gmail.com>
2020-01-29 20:09:26 +0100

    added some maths functions in the utils module

--- modules/utils.js
+++ modules/utils.js
@@ -16,4 +16,51 @@ function range(first, last, step) {
     return [...Array(size).keys()].map(i => i * step + first);
 }
 
-export { range };
+function norm(vector) {
+    /*
+    Compute norm of vector.
+    Input:
+        -vector     [float, ...]
+    Output:
+        -           float
+    */
+    var value = 0;
+    for (var i = 0; i < vector.length; i++) {
+        value += vector[i] ^ 2;
+    }
+    return Math.sqrt(value);
+}
+
+function scalar_vector_multiplication(scalar, vector) {
+    /*
+    Multiply each member of a vector by a scalar.
+    Input:
+        -scalar         float
+        -vector         [float, ...]
+    Output:
+        -output_vector  [float, ...]
+    */
+    var output_vector = [];
+    for (var i = 0; i < vector.length; i++) {
+        output_vector.push(vector[i] * scalar);
+    }
+    return output_vector;
+}
+
+function add_vectors_elementwise(vector1, vector2) {
+    /*
+    Add two vectors elementwise.
+    Input:
+        -vector1        [float, ...]
+        -vector2        [float, ...]
+    Output:
+        -output_vector  [float, ...]
+    */
+    var output_vector = [];
+    for (var i = 0; i < vector1.length; i++) {
+        output_vector.push(vector1[i] + vector2[i]);
+    }
+    return output_vector;
+}
+
+export { range, norm, scalar_vector_multiplication, add_vectors_elementwise };
